9c0e10acfffb95b7e5dc392aecba7b1f36fdfef7
Ivan19012 <ivan.minaew2018@yandex.ru>
2025-09-03 19:02:55 +0300

    m

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -15,7 +15,7 @@ const NotFound = () => {
     <div className="min-h-screen flex items-center justify-center bg-background">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-muted-foreground mb-4">Страница не найдена</p>
+        <p className="text-xl text-muted-foreground mb-4">Страница не найдена вот блин</p>
         <a href="/" className="story-link">
           На главную
         </a>
