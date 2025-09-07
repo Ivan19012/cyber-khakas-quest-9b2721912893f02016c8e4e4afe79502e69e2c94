14669ca104737640dfc9ba596be21a54e3c29609
Ivan19012 <ivan.minaew2018@yandex.ru>
2025-09-07 12:04:40 +0300

    mobile

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode, command }) => ({
   base: "/",
   server: {
     host: "0.0.0.0",
@@ -16,6 +16,20 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",         // Папка для сборки
+    emptyOutDir: true,      // Очистка dist перед сборкой
+    chunkSizeWarningLimit: 800,  // Можно поднять лимит предупреждения по размеру чанков
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Выделить зависимости в отдельный чанковый файл vendor.js
+          }
+        },
+      },
     },
   },
 }));
